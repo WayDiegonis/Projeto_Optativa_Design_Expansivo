348de6502fd6889406b4cada557147497036b758
root <diegoneveslourenco@gmail.com>
2019-09-18 21:59:43 -0300

    Colocação de uma nova forma

---
+++ paginas/form.php
@@ -0,0 +1,12 @@
+	<form action="" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Endereço de email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Seu email">
+    <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" name="senha" id="exampleInputPassword1" placeholder="Senha">
+  </div>
+  <div class="form-group form-check">
+  <button type="submit" class="btn btn-primary">Enviar</button>
